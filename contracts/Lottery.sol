9778eb350730f6bd136a1f3ed099618533e7b21a
Jay Landers <jay@jaybirdgroup.net>
2021-12-31 01:02:22 -0700

    added functions to lottery contract and randomness through chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,9 +4,12 @@ pragma solidity >=0.6.6;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public recentRandom;
     uint256 usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
     enum LOTTERY_STATE {
@@ -15,11 +18,21 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
 
-    constructor(address _priceFeedAddress) {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * 10**18;
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -45,5 +58,40 @@ contract Lottery is Ownable {
         lottery_state = LOTTERY_STATE.OPEN;
     }
 
-    function endLottery() public onlyOwner {}
+    function endLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "Lottery does not exist yet"
+        ); //check lottery is opne
+
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER; //change state so other functions cant be called
+
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough link to fulfill request"
+        ); //check that contract can pay for rand num
+
+        requestRandomness(keyhash, fee); //get rand num
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "Winner cannot be chosen yet"
+        );
+        require(
+            _randomness > 0 && _randomness != recentRandom,
+            "Random not found"
+        ); //ensure random num was returned
+        uint256 randomWinner = _randomness % players.length; //random num --> array index
+        recentWinner = players[randomWinner]; //set winner
+        recentWinner.transfer(address(this).balance); //pay
+        //Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        recentRandom = _randomness; //to check next num against
+    }
 }
