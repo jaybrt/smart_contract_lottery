3d8f581ba5fb59d441e77a4ea9a5afaefc2cc7f4
Jay Landers <jay@jaybirdgroup.net>
2021-12-30 18:34:46 -0700

    added lottery state and ownership requirements

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,20 +3,29 @@
 pragma solidity >=0.6.6;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) {
         usdEntryFee = 50 * 10**18;
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN, "Lotery not open");
         //$50 min
-
+        require(msg.value >= getEnteranceFee(), "Not enough ETH");
         players.push(payable(msg.sender));
     }
 
@@ -28,7 +37,13 @@ contract Lottery {
         return ethEnteranceFee;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Cannot start new lottery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {}
 }
